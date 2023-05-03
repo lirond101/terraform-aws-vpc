91443bbf249ace08f2f1480082ed176b2dfe6fb7
lirond101 <lirond101@gmail.com>
2023-05-03 11:03:23 +0000

    reorganize and update vars

--- main.tf
+++ main.tf
@@ -2,7 +2,9 @@ resource "aws_vpc" "vpc" {
   cidr_block           = var.vpc_cidr_block
   enable_dns_hostnames = var.enable_dns_hostnames
 
-  tags = var.common_tags
+  tags = merge(var.common_tags, {
+    Name = "${var.name_prefix}-vpc"
+  })
 }
 
 resource "aws_subnet" "public_subnet" {
@@ -10,14 +12,15 @@ resource "aws_subnet" "public_subnet" {
     aws_vpc.vpc,
   ]
 
-  count                   = 2
+  count                   = length(var.public_subnets)
   cidr_block              = var.public_subnets[count.index]
   availability_zone       = var.availability_zone[count.index]
   vpc_id                  = aws_vpc.vpc.id
-  map_public_ip_on_launch = var.map_public_ip_on_launch
-  tags = {
-    Name = "public-subnet-${count.index+1}"
-  }
+  map_public_ip_on_launch = true
+  tags = merge(var.common_tags, var.public_subnet_tags, {
+    Name = "${var.name_prefix}-public-${var.availability_zone[count.index]}",
+    Tier = "Public"
+  })
 }
 
 resource "aws_subnet" "private_subnet" {
@@ -25,13 +28,14 @@ resource "aws_subnet" "private_subnet" {
     aws_vpc.vpc,
   ]
 
-  count             = 2
+  count             = length(var.private_subnets)
   cidr_block        = var.private_subnets[count.index]
   availability_zone = var.availability_zone[count.index]
   vpc_id            = aws_vpc.vpc.id
-  tags = {
-    Name = "private-subnet-${count.index+1}"
-  }
+  tags = merge(var.common_tags, var.private_subnet_tags, {
+    Name = "${var.name_prefix}-private-${var.availability_zone[count.index]}",
+    Tier = "Private"
+  })
 }
 
 resource "aws_internet_gateway" "igw" {
@@ -40,9 +44,9 @@ resource "aws_internet_gateway" "igw" {
   ]
 
   vpc_id = aws_vpc.vpc.id
-  tags = {
-    Name = "${var.naming_prefix}-igw"
-  }
+  tags = merge(var.common_tags, {
+    Name = "${var.name_prefix}-igw"
+  })
 }
 
 resource "aws_route_table" "IG_route_table" {
@@ -52,63 +56,54 @@ resource "aws_route_table" "IG_route_table" {
   ]
 
   vpc_id = aws_vpc.vpc.id
-  # route {
-  #   cidr_block = "0.0.0.0/0"
-  #   gateway_id = aws_internet_gateway.igw.id
-  # }
-  tags = {
-    Name = "${var.naming_prefix}-IG-rt"
-  }
+  tags = merge(var.common_tags, {
+    Name = "${var.name_prefix}-IG-rt"
+  })
 }
 
 resource "aws_route" "ngw-default-route" {
     depends_on = [
     aws_route_table.IG_route_table,
   ]
+
   route_table_id         = aws_route_table.IG_route_table.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.igw.id
 }
 
-# associate route table to public subnet
 resource "aws_route_table_association" "associate_routetable_to_public_subnet" {
   depends_on = [
     aws_subnet.public_subnet,
     aws_route_table.IG_route_table,
   ]
 
-  count          = 2
+  count          = length(var.public_subnets)
   route_table_id = aws_route_table.IG_route_table.id
   subnet_id      = aws_subnet.public_subnet.*.id[count.index]
 }
 
-# elastic ip
 resource "aws_eip" "elastic_ip" {
-  count = 2
+  count = length(var.public_subnets)
   vpc   = true
-
-  tags = {
-    Name = "${var.naming_prefix}-eip-${count.index+1}"
-  }
+  tags = merge(var.common_tags, {
+    Name = "${var.name_prefix}-eip-${count.index+1}"
+  })
 }
 
-# NAT gateway
 resource "aws_nat_gateway" "nat_gateway" {
   depends_on = [
     aws_subnet.public_subnet,
     aws_eip.elastic_ip,
   ]
 
-  count         = 2
+  count         = length(var.public_subnets)
   allocation_id = aws_eip.elastic_ip.*.id[count.index]
   subnet_id     = aws_subnet.public_subnet.*.id[count.index]
-
-  tags = {
-    Name = "${var.naming_prefix}-nat-gateway-${count.index+1}"
-  }
+  tags = merge(var.common_tags, {
+    Name = "${var.name_prefix}-nat-gateway-${count.index+1}"
+  })
 }
 
-# route table with target as NAT gateway
 resource "aws_route_table" "NAT_route_table" {
   depends_on = [
     aws_vpc.vpc,
@@ -116,35 +111,30 @@ resource "aws_route_table" "NAT_route_table" {
   ]
 
   vpc_id = aws_vpc.vpc.id
-  count  = 2
-  # route {
-  #   cidr_block = "0.0.0.0/0"
-  #   gateway_id = aws_nat_gateway.nat_gateway.*.id[count.index]
-  # }
-
-  tags = {
-    Name = "${var.naming_prefix}-NAT-rt-${count.index+1}"
-  }
+  count  = length(var.public_subnets)
+  tags = merge(var.common_tags, {
+    Name = "${var.name_prefix}-NAT-rt-${count.index+1}"
+  })
 }
 
 resource "aws_route" "nat-default-route" {
   depends_on = [
     aws_route_table.NAT_route_table,
   ]
-  count  = 2
+
+  count                  = length(var.public_subnets)
   route_table_id         = aws_route_table.NAT_route_table.*.id[count.index]
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_nat_gateway.nat_gateway.*.id[count.index]
 }
 
-# associate route table to private subnet
 resource "aws_route_table_association" "associate_routetable_to_private_subnet" {
   depends_on = [
     aws_subnet.private_subnet,
     aws_route_table.NAT_route_table,
   ]
 
-  count          = 2
+  count          = length(var.public_subnets)
   subnet_id      = aws_subnet.private_subnet.*.id[count.index]
   route_table_id = aws_route_table.NAT_route_table.*.id[count.index]
 }
--- variables.tf
+++ variables.tf
@@ -1,31 +1,16 @@
-variable "naming_prefix" {
+variable "name_prefix" {
   type        = string
-  description = "Naming prefix for resources"
-  default     = "vpc-assig"
+  description = "Name prefix for resources"
 }
 
 variable "enable_dns_hostnames" {
   type        = bool
   description = "Enable DNS hostnames in VPC"
-  default     = true
 }
 
 variable "vpc_cidr_block" {
   type        = string
   description = "Base CIDR Block for VPC"
-  default     = "10.1.0.0/16"
-}
-
-variable "vpc_subnet_count" {
-  type        = number
-  description = "Number of subnets to create in VPC"
-  default     = 2
-}
-
-variable "map_public_ip_on_launch" {
-  type        = bool
-  description = "Map a public IP address for Subnet instances"
-  default     = true
 }
 
 variable "public_subnets" {
@@ -43,14 +28,26 @@ variable "availability_zone" {
   description = "Desired AZs as list of strings"
 }
 
-variable "company" {
-  type        = string
-  description = "Company name for resource tagging"
-  default     = "Opsschool"
-}
-
 variable "common_tags" {
   type        = map(string)
   description = "Map of tags to be applied to all resources"
+  default     = {}
+}
+
+# variable "vpc_tags" {
+#   type        = map(string)
+#   description = "Map of tags to be applied to public subnets"
+#   default     = {}
+# }
+
+variable "public_subnet_tags" {
+  type        = map(string)
+  description = "Map of tags to be applied to public subnets"
+  default     = {}
+}
+
+variable "private_subnet_tags" {
+  type        = map(string)
+  description = "Map of tags to be applied to private subnets"
   default     = {}
 }
